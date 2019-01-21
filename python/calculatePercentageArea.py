6f3b95a2208c04742f5c42fdcf003eab9c3451f7
Marrit <marrit.leenstra@wur.nl>
2019-01-21 17:23:04 +0100

    function to intersect water and parcels

--- python/calculatePercentageArea.py
+++ python/calculatePercentageArea.py
@@ -3,11 +3,14 @@
 # Authors: Anne-Juul Welsink and Marrit Leenstra
 # Date: 21-01-2019
 
+import geopandas as gpd
+
 def calculatePercentageArea(GDF1, GDF2):
     """calculate the area of the water per parcel 
     
+    :GDF1: first geodataframe
+    :GDF2: second geodataframe
+    return: intersecting areas
     """
-    intersection = gpd.overlay(GDF1, GDF2, how = 'identity') # or other?
-    print(intersection.head())
-    
-    # todo: some calculation
+    intersection = gpd.overlay(GDF1, GDF2, how = 'intersection')
+    return intersection
